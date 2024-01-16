11115f46949a7305bcabfd89578928f95fe21265
wcgraupmann <141872356+wcgraupmann@users.noreply.github.com>
2024-01-16 11:26:52 -0600

    added logo to sign in

--- cookbook/src/components/Signin/Signin.js
+++ cookbook/src/components/Signin/Signin.js
@@ -1,4 +1,5 @@
 import React from "react";
+import logo from "../../logo-color.png";
 
 class Signin extends React.Component {
   constructor(props) {
@@ -16,11 +17,11 @@ class Signin extends React.Component {
 
   render() {
     return (
-      <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div class="flex min-h-full flex-col justify-center px-6 pb-12 lg:px-8">
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
-            class="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            class="mx-auto h-30 w-auto border rounded"
+            src={logo}
             alt="Your Company"
           />
           <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
--- cookbook/src/components/Welcome/Welcome.js
+++ cookbook/src/components/Welcome/Welcome.js
@@ -1,45 +1,43 @@
 import React from "react";
 import logo from "../../../src/logo-color.png";
 
-class Welcome extends React.Component {
-  render() {
-    return (
-      <div>
-        <div className="flex justify-center">
-          <img
-            class="h-60 w-80 object-contain rounded-md"
-            src={logo}
-            alt="description"
-          />
-        </div>
-        <div className="m-5 p-10 border border-black flex bg-stone-300 rounded">
-          <p className="flex-auto border border-black bg-stone-200 rounded basis-1/4">
-            How it Works
-          </p>
-          <p className="flex-auto border border-black ml-5 bg-stone-200 rounded basis-1/2">
-            Save your favorite new recipes and share them with your Cookbook
-            Club!
-          </p>
-        </div>
-        <div className="m-5 p-10 border border-black flex bg-stone-300 rounded">
-          <p className="flex-auto border border-black bg-stone-200 rounded basis-1/4">
-            Features
-          </p>
-          <p className="flex-auto border border-black ml-5 bg-stone-200 rounded basis-1/2">
-            Create or join a Cookbook Club with family or friends and inspire
-            each other to try new dishes
-          </p>
-        </div>
-        <div className="m-5 p-10 border border-black flex bg-stone-300 rounded">
-          <p className="flex-auto border border-black bg-stone-200 rounded basis-1/4">
-            Explain community
-          </p>
-          <p className="flex-auto border border-black ml-5 bg-stone-200 rounded basis-1/2">
-            Build a community or join a club that share your culinary interests
-          </p>
-        </div>
+const Welcome = () => {
+  return (
+    <div>
+      <div className="flex justify-center">
+        <img
+          class="h-60 w-80 object-contain rounded-md"
+          src={logo}
+          alt="description"
+        />
       </div>
-    );
-  }
-}
+      <div className="m-5 p-10 border border-black flex bg-stone-300 rounded">
+        <p className="flex-auto border border-black bg-stone-200 rounded basis-1/4">
+          How it Works
+        </p>
+        <p className="flex-auto border border-black ml-5 bg-stone-200 rounded basis-1/2">
+          Save your favorite new recipes and share them with your Cookbook Club!
+        </p>
+      </div>
+      <div className="m-5 p-10 border border-black flex bg-stone-300 rounded">
+        <p className="flex-auto border border-black bg-stone-200 rounded basis-1/4">
+          Features
+        </p>
+        <p className="flex-auto border border-black ml-5 bg-stone-200 rounded basis-1/2">
+          Create or join a Cookbook Club with family or friends and inspire each
+          other to try new dishes
+        </p>
+      </div>
+      <div className="m-5 p-10 border border-black flex bg-stone-300 rounded">
+        <p className="flex-auto border border-black bg-stone-200 rounded basis-1/4">
+          Explain community
+        </p>
+        <p className="flex-auto border border-black ml-5 bg-stone-200 rounded basis-1/2">
+          Build a community or join a club that share your culinary interests
+        </p>
+      </div>
+    </div>
+  );
+};
+
 export default Welcome;
