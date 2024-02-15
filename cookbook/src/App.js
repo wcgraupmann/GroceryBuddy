a18e8f04a8fbb4f698572506e32283e846b8fc5a
wcgraupmann <141872356+wcgraupmann@users.noreply.github.com>
2024-02-15 11:00:10 -0600

    adding useEffect hook to gather user info

--- cookbook/src/App.js
+++ cookbook/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Navigation from "./components/Navigation/Navigation";
 import Signin from "./components/Signin/Signin";
 import Register from "./components/Register/Register";
@@ -7,84 +7,118 @@ import Cookbook from "./components/Cookbook/Cookbook";
 import Welcome from "./components/Welcome/Welcome";
 import Hub from "./components/Hub/Hub";
 
-const initalState = {
-  isSignedIn: false,
-  route: "home",
-  user: {
-    id: "",
-    name: "",
-    email: "",
-    entries: 0,
-    joined: "",
-  },
-};
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isSignedIn: false,
-      route: "home",
-      user: {
-        id: "",
-        name: "",
-        email: "",
-        entries: 0,
-        joined: "",
-      },
-    };
-  }
+// const initalState = {
+//   isSignedIn: false,
+//   route: "home",
+//   user: {
+//     id: "",
+//     name: "",
+//     email: "",
+//     entries: 0,
+//     joined: "",
+//   },
+// };
 
-  // componentDidMount() {
-  //   fetch("http://localhost:3000")
-  //     .then((response) => response.json())
-  //     .then(console.log);
+const App = () => {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     isSignedIn: false,
+  //     route: "home",
+  //     user: {
+  //       id: "",
+  //       name: "",
+  //       email: "",
+  //       entries: 0,
+  //       joined: "",
+  //     },
+  //   };
   // }
+  const [userInfo, setUserInfo] = useState(null);
+  const [route, setRoute] = useState("home");
+  const [isSignedIn, setIsSignedIn] = useState(false);
 
-  loadUser = (data) => {
-    this.setState({
-      user: {
-        id: data.id,
-        name: data.name,
-        email: data.email,
-        entries: data.entries,
-        joined: data.joined,
-      },
-    });
-  };
+  // get user name
+  // useEffect(() => {
+  //   const getUser = async () => {
+  //     try {
+  //       const token = localStorage.getItem("token");
 
-  onRouteChange = (route) => {
+  //       if (!token) {
+  //         throw new Error("No token found");
+  //       }
+
+  //       const response = fetch("http://localhost:3000/protected", {
+  //         method: "post",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //           Authorization: `Bearer ${token}`,
+  //         },
+  //       });
+
+  //       if (!response.ok) {
+  //         throw new Error("Failed to fetch user data");
+  //       }
+
+  //       const data = await response.json();
+  //       setUserInfo(data);
+  //     } catch (error) {
+  //       console.error("Error signing in:", error.message);
+  //     }
+  //   };
+
+  //   getUser();
+  // }, []);
+  // console.log(userInfo);
+
+  // loadUser = (data) => {
+  //   this.setState({
+  //     user: {
+  //       id: data.id,
+  //       name: data.name,
+  //       email: data.email,
+  //       entries: data.entries,
+  //       joined: data.joined,
+  //     },
+  //   });
+  // };
+
+  // const onRouteChange = (route) => {
+  //   if (route === "signout") {
+  //     this.setState(initalState);
+  //   } else if (route === "main") {
+  //     this.setState({ isSignedIn: true });
+  //     console.log("isSignedIn set to true");
+  //   }
+  //   this.setState({ route: route });
+  //   console.log(`route is set to: ${route}`);
+  // };
+  const onRouteChange = (route) => {
     if (route === "signout") {
-      this.setState(initalState);
+      setIsSignedIn(false);
     } else if (route === "main") {
-      this.setState({ isSignedIn: true });
-      console.log("isSignedIn set to true");
+      setIsSignedIn(true);
     }
-    this.setState({ route: route });
-    console.log(`route is set to: ${route}`);
+    setRoute(route);
   };
 
-  render() {
-    const { isSignedIn, route } = this.state;
-    return (
-      <div className="App">
-        <Navigation
-          isSignedIn={isSignedIn}
-          onRouteChange={this.onRouteChange}
-        />
-        {route === "home" ? (
-          <Welcome />
-        ) : route === "main" ? (
-          <Hub onRouteChange={this.onRouteChange} />
-        ) : route === "cookbook" ? (
-          <Cookbook />
-        ) : route === "signin" ? (
-          <Signin onRouteChange={this.onRouteChange} onSignIn={this.loadUser} />
-        ) : (
-          <Register onRouteChange={this.onRouteChange} />
-        )}
-      </div>
-    );
-  }
-}
+  // const { isSignedIn, route } = this.state;
+  return (
+    <div className="App">
+      <Navigation isSignedIn={isSignedIn} onRouteChange={onRouteChange} />
+      {route === "home" ? (
+        <Welcome />
+      ) : route === "main" ? (
+        <Hub onRouteChange={onRouteChange} />
+      ) : route === "cookbook" ? (
+        <Cookbook />
+      ) : route === "signin" ? (
+        <Signin onRouteChange={onRouteChange} />
+      ) : (
+        <Register onRouteChange={onRouteChange} />
+      )}
+    </div>
+  );
+};
 
 export default App;
--- cookbook/src/components/Hub/Hub.js
+++ cookbook/src/components/Hub/Hub.js
@@ -1,41 +1,40 @@
 import React from "react";
 import logo from "../../../src/logo-color.png";
 
-class Hub extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const Hub = ({ onChangePage }) => {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {};
+  // }
 
   onChangePage = () => {
     this.props.onRouteChange("cookbook");
   };
 
-  render() {
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <h1>User's Profile Page</h1>
+        <h1>User's Profile Page</h1>
+      </div>
+      <div className="flex">
+        <div className="border border-black mt-8 ml-8 mr-2 p-8 bg-slate-200">
+          <button onClick={onChangePage}>My Recipes</button>
         </div>
-        <div className="flex">
-          <div className="border border-black mt-8 ml-8 mr-2 p-8 bg-slate-200">
-            <button onClick={this.onChangePage}>My Recipes</button>
-          </div>
-          <div className="bg-[#ffe4c4] my-5 mx-16 p-16 border border-black rounded flex mt-8">
-            <img
-              className="h-60 w-80 object-contain rounded-md"
-              src={logo}
-              alt="description"
-            />
-            <div className="ml-32 pl-8 pr-80 border border-black rounded-md flex flex-col justify-evenly">
-              <p>My Profile</p>
-              <p>My Profile</p>
-              <p>My Profile</p>
-            </div>
+        <div className="bg-[#ffe4c4] my-5 mx-16 p-16 border border-black rounded flex mt-8">
+          <img
+            className="h-60 w-80 object-contain rounded-md"
+            src={logo}
+            alt="description"
+          />
+          <div className="ml-32 pl-8 pr-80 border border-black rounded-md flex flex-col justify-evenly">
+            <p>My Profile</p>
+            <p>My Profile</p>
+            <p>My Profile</p>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 export default Hub;
