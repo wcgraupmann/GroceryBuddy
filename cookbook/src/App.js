934a643b383c9cfa761b70db75c65301a7564510
wcgraupmann <141872356+wcgraupmann@users.noreply.github.com>
2024-02-21 14:54:55 -0600

    configured fetch to backend with jwt token

--- cookbook/src/App.js
+++ cookbook/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Navigation from "./components/Navigation/Navigation";
 import Signin from "./components/Signin/Signin";
 import Register from "./components/Register/Register";
@@ -7,92 +7,12 @@ import Cookbook from "./components/Cookbook/Cookbook";
 import Welcome from "./components/Welcome/Welcome";
 import Hub from "./components/Hub/Hub";
 
-// const initalState = {
-//   isSignedIn: false,
-//   route: "home",
-//   user: {
-//     id: "",
-//     name: "",
-//     email: "",
-//     entries: 0,
-//     joined: "",
-//   },
-// };
-
 const App = () => {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     isSignedIn: false,
-  //     route: "home",
-  //     user: {
-  //       id: "",
-  //       name: "",
-  //       email: "",
-  //       entries: 0,
-  //       joined: "",
-  //     },
-  //   };
-  // }
-  const [userInfo, setUserInfo] = useState(null);
   const [route, setRoute] = useState("home");
   const [isSignedIn, setIsSignedIn] = useState(false);
 
-  // get user name
-  // useEffect(() => {
-  //   const getUser = async () => {
-  //     try {
-  //       const token = localStorage.getItem("token");
-
-  //       if (!token) {
-  //         throw new Error("No token found");
-  //       }
-
-  //       const response = fetch("http://localhost:3000/protected", {
-  //         method: "post",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //           Authorization: `Bearer ${token}`,
-  //         },
-  //       });
-
-  //       if (!response.ok) {
-  //         throw new Error("Failed to fetch user data");
-  //       }
-
-  //       const data = await response.json();
-  //       setUserInfo(data);
-  //     } catch (error) {
-  //       console.error("Error signing in:", error.message);
-  //     }
-  //   };
-
-  //   getUser();
-  // }, []);
-  // console.log(userInfo);
-
-  // loadUser = (data) => {
-  //   this.setState({
-  //     user: {
-  //       id: data.id,
-  //       name: data.name,
-  //       email: data.email,
-  //       entries: data.entries,
-  //       joined: data.joined,
-  //     },
-  //   });
-  // };
-
-  // const onRouteChange = (route) => {
-  //   if (route === "signout") {
-  //     this.setState(initalState);
-  //   } else if (route === "main") {
-  //     this.setState({ isSignedIn: true });
-  //     console.log("isSignedIn set to true");
-  //   }
-  //   this.setState({ route: route });
-  //   console.log(`route is set to: ${route}`);
-  // };
+  // function to change the webpage to intended route
+  // also sets signin boolean on signin / signout
   const onRouteChange = (route) => {
     if (route === "signout") {
       setIsSignedIn(false);
@@ -102,7 +22,6 @@ const App = () => {
     setRoute(route);
   };
 
-  // const { isSignedIn, route } = this.state;
   return (
     <div className="App">
       <Navigation isSignedIn={isSignedIn} onRouteChange={onRouteChange} />
--- cookbook/src/components/Hub/Hub.js
+++ cookbook/src/components/Hub/Hub.js
@@ -1,24 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../../../src/logo-color.png";
 
-const Hub = ({ onChangePage }) => {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {};
-  // }
+const Hub = ({ onRouteChange }) => {
+  const [userInfo, setUserInfo] = useState(null);
+  // fetch user info and store in useState
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        if (!token) {
+          throw new Error("No token found");
+        }
+        const response = await fetch("http://localhost:3000/protected", {
+          method: "get",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        if (!response.ok) {
+          throw new Error("Failed to fetch user data");
+        }
+        const data = await response.json();
+        setUserInfo(data);
+      } catch (error) {
+        console.error("Error signing in:", error.message);
+      }
+    };
+    getUser();
+  }, []);
 
-  onChangePage = () => {
-    this.props.onRouteChange("cookbook");
-  };
+  // console.log(userInfo);
 
   return (
     <div>
       <div>
-        <h1>User's Profile Page</h1>
+        <h1>Profile Page</h1>
       </div>
       <div className="flex">
         <div className="border border-black mt-8 ml-8 mr-2 p-8 bg-slate-200">
-          <button onClick={onChangePage}>My Recipes</button>
+          <button onClick={() => onRouteChange("cookbook")}>My Recipes</button>
         </div>
         <div className="bg-[#ffe4c4] my-5 mx-16 p-16 border border-black rounded flex mt-8">
           <img
